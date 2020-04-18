d0bf17d0701927f9274bcfd408e937d8c0fccde3
wsduarte <ws.duarte@uniandes.edu.co>
2020-04-17 20:51:57 -0500

    Validación del usuario desde base de datos

--- handlegenerator.js
+++ handlegenerator.js
@@ -17,21 +17,21 @@ class HandlerGenerator {
 
     // Si se especifico un usuario y contraseña, proceda con la validación
     // de lo contrario, un mensaje de error es retornado
-    if( username && password ) {
+    if( username_in && password_in ) {
 
         let cliente = new UsuariosCollection();
         cliente.getAll({
             login: username_in,
             pw: password_in
-        }).then(res => {
-            console.log(res);
+        }).then(info_usuario => {
             /* ---- */
             // Si los usuarios y las contraseñas coinciden, proceda con la generación del token
             // de lo contrario, un mensaje de error es retornado
-            if( /*username === mockedUsername && password === mockedPassword*/ info_usuario ) {
+            /*username === mockedUsername && password === mockedPassword*/ 
+            if( info_usuario.length > 0 && info_usuario[0] != null && info_usuario[0] != undefined ) {
                 
                 // Se genera un nuevo token para el nombre de usuario el cuál expira en 24 horas
-                let token = jwt.sign( { username: username },
+                let token = jwt.sign( { username: username_in },
                 config.secret, { expiresIn: '24h' } );
                 
                 // Retorna el token el cuál debe ser usado durante las siguientes solicitudes
@@ -44,7 +44,8 @@ class HandlerGenerator {
             } else {
                 
                 // El error 403 corresponde a Forbidden (Prohibido) de acuerdo al estándar HTTP
-                res.send( 403 ).json( {
+                res.statusCode = 403;
+                res.json( {
                 success: false,
                 message: 'Incorrect username or password'
                 } );
